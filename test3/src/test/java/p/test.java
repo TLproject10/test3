4a6b8a2c3a2c99c368b8963f3fda460438009857
Qspider3rs <diganta.sdp@gmail.com>
2020-07-24 21:02:41 +0530

    first

---
+++ test3/src/test/java/p/test.java
@@ -0,0 +1,24 @@
+package p;
+import org.testng.annotations.Test;
+
+
+
+public class test 
+{
+
+	
+	@Test
+	public void pre()
+	{
+		System.out.println("this is test class");
+		String value = System.getProperty("browserName");
+		System.out.println(value);
+	
+		String value2 = System.getProperty("platformName");
+		System.out.println(value2);
+		
+		String value3 = System.getProperty("urlName");
+		System.out.println(value3);
+	
+	}
+}
